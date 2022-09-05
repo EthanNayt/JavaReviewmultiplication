0aa8e1426f592584404b8a9894e7116c4d33fa5c
EthanNayt <ethanaung260@gmail.com>
2022-09-05 13:27:07 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int n1, n2, total;
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Enter 1st number");
+        n1 = sc.nextInt();
+
+
+        System.out.println("Enter 2nd number");
+        n2 = sc.nextInt();
+
+        total = n1 * n2;
+                System.out.println(total);
+
+    }
+
+}
